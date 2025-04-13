3802645b6ceed5fa4cef3589b85abee4e0aee813
deepakdevswaroop <deepakdevswaroop@gmail.com>
2025-04-13 16:41:02 -0700

    Update variables.tf

--- terraform-infra/variables.tf
+++ terraform-infra/variables.tf
@@ -1,16 +1,61 @@
-variable "location" {}
-variable "resource_group_name" {}
-variable "admin_username" {}
-variable "ssh_public_key" {}
+variable "location" {
+  description = "Azure location"
+  type        = string
+}
 
-# VM-specific
-variable "vm_count" {}
-variable "vm_size" {}
-variable "vm_name_prefix" {}
+variable "resource_group_name" {
+  description = "Resource Group name"
+  type        = string
+}
 
-# K8s-specific
-variable "cluster_name" {}
-variable "dns_prefix" {}
-variable "kubernetes_version" {}
-variable "node_count" {}
-variable "node_vm_size" {}
+variable "admin_username" {
+  description = "SSH admin username"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "Path to your SSH public key"
+  type        = string
+}
+
+# VM module inputs
+variable "vm_count" {
+  description = "Number of VMs to create"
+  type        = number
+}
+
+variable "vm_size" {
+  description = "Size of VM instances"
+  type        = string
+}
+
+variable "vm_name_prefix" {
+  description = "Prefix for VM names"
+  type        = string
+}
+
+# Kubernetes module inputs
+variable "cluster_name" {
+  description = "Kubernetes cluster name"
+  type        = string
+}
+
+variable "dns_prefix" {
+  description = "DNS prefix for the cluster"
+  type        = string
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version to deploy"
+  type        = string
+}
+
+variable "node_count" {
+  description = "Number of nodes in Kubernetes cluster"
+  type        = number
+}
+
+variable "node_vm_size" {
+  description = "VM size for Kubernetes worker nodes"
+  type        = string
+}
