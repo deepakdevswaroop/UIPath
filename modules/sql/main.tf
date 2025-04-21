8bc43c18ab8d722ccdfca4e5725a26c388e44738
deepakdevswaroop <deepakdevswaroop@gmail.com>
2025-04-21 09:17:17 -0700

    Update main.tf

--- modules/sql/main.tf
+++ modules/sql/main.tf
@@ -1,26 +1,30 @@
-resource "azurerm_mssql_server" "sql_server" {
+resource "azurerm_sql_server" "sql_server" {
   name                         = var.sql_server_name
-  resource_group_name          = var.resource_group_name
   location                     = var.location
+  resource_group_name          = var.resource_group_name
   version                      = "12.0"
-  administrator_login          = var.admin_username
-  administrator_login_password = var.admin_password
-
-  identity {
-    type = "SystemAssigned"
-  }
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
 
   tags = {
-    environment = "dev"
+    Environment = "Terraform"
   }
 }
 
-resource "azurerm_mssql_database" "sql_db" {
-  name               = var.sql_database_name
-  server_id          = azurerm_mssql_server.sql_server.id
-  collation          = "SQL_Latin1_General_CP1_CI_AS"
-  sku_name           = var.sku_name
-  max_size_gb        = var.max_size_gb
-  zone_redundant     = false
-  read_scale         = false
+resource "azurerm_sql_database" "sql_db" {
+  name                = var.sql_database_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  server_name         = azurerm_sql_server.sql_server.name
+  sku_name            = var.sql_sku_name
+  max_size_gb         = var.sql_max_size_gb
+}
+
+resource "azurerm_public_ip" "sql_public_ip" {
+  count               = var.assign_public_ip_to == "sql" ? 1 : 0
+  name                = var.public_ip_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
