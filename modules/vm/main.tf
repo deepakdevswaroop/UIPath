7bce91cc1e3eadd8d6f4e2f2c26957dcce03c4a6
deepakdevswaroop <deepakdevswaroop@gmail.com>
2025-04-21 09:13:54 -0700

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,17 +1,30 @@
-resource "random_id" "vm_suffix" {
-  byte_length = 2
-  count       = var.vm_count
+resource "azurerm_network_interface" "vm_nic" {
+  count               = var.vm_count
+  name                = "${var.vm_name_prefix}-nic-${count.index}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+
+    public_ip_address_id = var.assign_public_ip_to == "vm" && count.index == 0
+      ? azurerm_public_ip.vm_public_ip[0].id
+      : null
+  }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
   count               = var.vm_count
-  name                = "${var.name_prefix}-${count.index + 1}-${random_id.vm_suffix[count.index].hex}"
+  name                = "${var.vm_name_prefix}-${count.index}"
   location            = var.location
   resource_group_name = var.resource_group_name
   size                = var.vm_size
   admin_username      = var.admin_username
-
-  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
+  network_interface_ids = [
+    azurerm_network_interface.vm_nic[count.index].id
+  ]
 
   admin_ssh_key {
     username   = var.admin_username
@@ -26,34 +39,16 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20_04-lts"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.name_prefix}-vnet"
-  address_space       = ["10.0.0.0/16"]
+resource "azurerm_public_ip" "vm_public_ip" {
+  count               = var.assign_public_ip_to == "vm" ? 1 : 0
+  name                = var.public_ip_name
   location            = var.location
   resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "${var.name_prefix}-subnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "nic" {
-  count               = var.vm_count
-  name                = "${var.name_prefix}-nic-${count.index}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
