f640ad5f09a1432cb37fd6103685e843b0246207
deepakdevswaroop <deepakdevswaroop@gmail.com>
2025-04-21 11:40:54 -0700

    Update variables.tf

--- modules/k8s/variables.tf
+++ modules/k8s/variables.tf
@@ -9,12 +9,12 @@ variable "resource_group_name" {
 }
 
 variable "admin_username" {
-  description = "Admin username"
+  description = "Admin username for the AKS Linux profile"
   type        = string
 }
 
 variable "ssh_public_key" {
-  description = "SSH public key path"
+  description = "SSH public key content"
   type        = string
 }
 
@@ -24,12 +24,12 @@ variable "cluster_name" {
 }
 
 variable "dns_prefix" {
-  description = "DNS prefix for AKS"
+  description = "DNS prefix for the AKS cluster"
   type        = string
 }
 
 variable "kubernetes_version" {
-  description = "Kubernetes version"
+  description = "Kubernetes version to deploy"
   type        = string
 }
 
@@ -39,16 +39,11 @@ variable "node_count" {
 }
 
 variable "node_vm_size" {
-  description = "VM size for node pool"
+  description = "VM size for AKS node pool"
   type        = string
 }
 
-variable "assign_public_ip_to" {
-  description = "Which resource to assign public IP"
-  type        = string
-}
-
-variable "public_ip_name" {
-  description = "Name of the shared public IP"
+variable "public_ip_id" {
+  description = "Public IP resource ID to associate"
   type        = string
 }
