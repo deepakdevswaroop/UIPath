d188e08f8d59d78d6ebeee2df4173b5d5f5a6b15
deepakdevswaroop <deepakdevswaroop@gmail.com>
2025-04-21 09:16:11 -0700

    Update outputs.tf

--- modules/k8s/outputs.tf
+++ modules/k8s/outputs.tf
@@ -1,8 +1,12 @@
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
 output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
 
-output "cluster_name" {
-  value = azurerm_kubernetes_cluster.aks.name
+output "k8s_public_ip" {
+  value = var.assign_public_ip_to == "k8s" ? azurerm_public_ip.k8s_public_ip[0].ip_address : null
 }
