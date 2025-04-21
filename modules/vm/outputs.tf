1e96f65fb1a4d4a5b8d8f49a992dcd66207c45f5
deepakdevswaroop <deepakdevswaroop@gmail.com>
2025-04-21 09:14:14 -0700

    Update outputs.tf

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -1,3 +1,7 @@
 output "vm_names" {
-  value = [for i in azurerm_linux_virtual_machine.vm : i.name]
+  value = [for vm in azurerm_linux_virtual_machine.vm : vm.name]
+}
+
+output "vm_public_ip" {
+  value = var.assign_public_ip_to == "vm" ? azurerm_public_ip.vm_public_ip[0].ip_address : null
 }
