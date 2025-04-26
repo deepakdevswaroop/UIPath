9b08bacadf5e8a6ae51eb2ec38a2d05795c11400
deepakdevswaroop <deepakdevswaroop@gmail.com>
2025-04-25 17:11:28 -0700

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vm_names" {
-  value = [for vm in azurerm_linux_virtual_machine.this : vm.name]
-}
-
-output "vm_private_ips" {
-  value = [for nic in azurerm_network_interface.this : nic.ip_configuration[0].private_ip_address]
-}
